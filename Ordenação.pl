a3e2fcf0a329420881033308fe5a9a95e9ddfe79
Leonardo Guilherme Fodi <32180418+leoguifo@users.noreply.github.com>
2020-04-27 17:52:15 -0300

    Correção nos metodos

--- Ordenação.pl
+++ Ordenação.pl
@@ -1,9 +1,9 @@
 % paciente(nome, idade, temp, cardiaca, resp, pressão, oxigênio, dispnéia, comorbidades)
-paciente(p1, 35, 36, 90, 15, 120, 100, false, 0). % sem risco, liberado (verde)
-paciente(p2, 75, 39, 110, 30, 120, 100, false, 1). % fica em observação (amarelo)
-paciente(p3, 65, 37.5, 105, 20, 120, 100, false, 1). % fica em observação (amarelo)
-paciente(p4, 22, 36, 100, 25, 90, 95, true, 0). % p_arterial e dispinéia grave (laranja)
-paciente(p5, 21, 36, 150, 35, 110, 100, false, 0). % freq_respiratória gravissímo (vermelho)
+paciente(p1, 35, 36, 90, 15, 120, 100, false, 0). % sem risco (verde)
+paciente(p2, 75, 39, 110, 30, 120, 100, false, 1). % 14 dias de observação (amarelo)
+paciente(p3, 65, 37.5, 105, 20, 120, 100, false, 1). % 14 dias de observação (amarelo)
+paciente(p4, 22, 36, 100, 25, 90, 95, true, 0). % p_arterial e dispinéia (laranja)
+paciente(p5, 21, 36, 150, 35, 110, 100, true, 0). % freq_res e dispinéia (vermelho)
 
 %Idade: Baixo risco(amarelo) = 60 a 79 anos
 idade(N):-paciente(N,I,_,_,_,_,_,_,_), I >= 60, paciente(N,I,_,_,_,_,_,_,_), I =< 79.
@@ -45,21 +45,19 @@ comorbidade(N):-paciente(N,_,_,_,_,_,_,_,C), C = 1.
 comorbidade2(N):-paciente(N,_,_,_,_,_,_,_,C), C >= 2.
 
 % Categorização
-% 
-%  Paciente sem risco, mas deve ficar em observação:
 medio(X):- idade(X), temperatura(X), cardiaca(X), respiratoria(X), comorbidade(X),
-    print(X), write(" Rico médio\n").
+    print(X), write(" Paciente de baixo risco\n").
 
 %  Paciente com risco, deve ser encaminhado para hospital:
 alto(X):-(idade2(X); temperatura2(X); respiratoria2(X); 
     pressao2(X); oxigenio(X); dispneia(X); 
     comorbidade2(X) ), print(X), 
-    write(" Risco alto\n").
+    write(" Paciente de alto risco\n").
 
 baixo(X):- not(idade(X)), not(idade2(X)), not(temperatura(X)), not(temperatura2(X)), 
 	not(cardiaca(X)), not(respiratoria(X)), not(respiratoria2(X)),
 	not(pressao(X)), not(pressao2(X)), not(oxigenio(X)), 
 	not(dispneia(X)), not(comorbidade(X)), not(comorbidade2(X)), print(X), 
-    write(" Risco baixo\n").
+    write(" Paciente sem risco\n").
 
 main(X):- medio(X); alto(X); baixo(X).
